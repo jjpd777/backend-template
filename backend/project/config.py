4f4a51f8bf50723eae7de445421f3e9ee1e847fc
Juanjo <jpalacio@busbot.us>
2018-07-09 15:42:41 -0400

    Added development, testing, and production to config.py. Also updated requirements.txt

--- backend/project/config.py
+++ backend/project/config.py
@@ -1,6 +1,23 @@
+db_uri ='postgres://papaya:greenpink@project-papaya.cab8do2hlpay.us-east-1.rds.amazonaws.com:5432/papaya'
 class BaseConfig:
     """Base configuration"""
     DEBUG = True
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'postgres://papaya:greenpink@project-papaya.cab8do2hlpay.us-east-1.rds.amazonaws.com:5432/papaya' 
+    SQLALCHEMY_DATABASE_URI = db_uri
+class DevelopmentConfig(BaseConfig):
+    """Development configuration"""
+    DEBUG = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = db_uri
+class TestingConfig(BaseConfig):
+    """Testing configuration"""
+    DEBUG = True
+    TESTING = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = db_uri
+class ProductionConfig(BaseConfig):
+    """Production configuration"""
+    DEBUG = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = db_uri
