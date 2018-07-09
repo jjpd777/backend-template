30d62000729fc52cd6b7b6ecd249caaa4ccf38ac
Juanjo <jpalacio@busbot.us>
2018-07-09 15:32:57 -0400

    Set up CustomJSONEnconder in utils.py

--- backend/project/__init__.py
+++ backend/project/__init__.py
@@ -1,42 +1,24 @@
 from flask import Flask
 from datetime import datetime, time
 from flask_sqlalchemy import SQLAlchemy
-from flask.json import JSONEncoder
+from project.utils import CustomJSONEncoder
 
 db = SQLAlchemy()
-class CustomJSONEncoder(JSONEncoder):
-    def default(self, obj):
-        try:
-            if isinstance(obj, datetime):
-                return obj.isoformat()
-            elif isinstance(obj, time):
-                return obj.isoformat()
-            iterable = iter(obj)
-        except TypeError:
-            pass
-        else:
-            return list(iterable)
-        return JSONEncoder.default(self, obj)
 
 def create_app():
-
     # instantiate the app
     app = Flask(__name__)
-
     # Set custom encoder
     app.json_encoder = CustomJSONEncoder
-
     # set config
     app.config.from_object('project.config.BaseConfig')
 
     db.init_app(app)
-
+    #Setting blueprints for interacting with DB objects
     from project.jobs import jobs_blueprint
     app.register_blueprint(jobs_blueprint)
     from project.users import users_blueprint
     app.register_blueprint(users_blueprint)
-    from project.rehive import rehive_blueprint
-    app.register_blueprint(rehive_blueprint)
 
     @app.route("/")
     def testing():
--- backend/project/utils.py
+++ backend/project/utils.py
@@ -1,4 +1,22 @@
 from sqlalchemy import inspect
+from flask.json import JSONEncoder
+from datetime import datetime, time
+
 def db_object_as_dict(obj):
     return {c.key: getattr(obj, c.key)
             for c in inspect(obj).mapper.column_attrs}
+
+class CustomJSONEncoder(JSONEncoder):
+    def default(self, obj):
+        try:
+            if isinstance(obj, datetime):
+                return obj.isoformat()
+            elif isinstance(obj, time):
+                return obj.isoformat()
+            iterable = iter(obj)
+        except TypeError:
+            pass
+        else:
+            return list(iterable)
+        return JSONEncoder.default(self, obj)
+
